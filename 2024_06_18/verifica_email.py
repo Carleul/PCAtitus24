e0a7179433aae2651ade12c86d8b1d45590a69e0
Carlo B. Guterres <164182231+Carleul@users.noreply.github.com>
2024-06-18 10:01:15 -0300

    mudanças

--- 2024_06_18/verifica_email.py
+++ 2024_06_18/verifica_email.py
@@ -2,9 +2,13 @@ def verifica_email(email):
     valida_email = []
     for letter in email:
         valida_email.append(letter)
+        if letter.isupper():
+            return False
     if len(valida_email) < 3:
         return False
     if not email.endswith('.com'):
+        return False
+    if email.count('.com') != 1:
         return False
     if email.count('@') != 1:
         return False
@@ -15,6 +19,7 @@ print(verifica_email('@') == False)
 print(verifica_email('@@') == False)
 print(verifica_email('abc@@abc.com') == False)
 print(verifica_email('abc@abc.edu') == False)
+print(verifica_email('ABC@abc.edu') == False)
 
 print(verifica_email('abc@abc.com') == True)
 print(verifica_email('a23@123.com') == True)
